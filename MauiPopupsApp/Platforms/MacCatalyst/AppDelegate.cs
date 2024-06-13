9049354e0b5e3c55474eb386bef0c2358f11010b
arunalu_kariyawasam <arunalu.kariyawasam@trapezegroup.com>
2024-06-13 18:10:57 -0400

    Add project files.

---
+++ MauiPopupsApp/Platforms/MacCatalyst/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace MauiPopupsApp
+{
+    [Register("AppDelegate")]
+    public class AppDelegate : MauiUIApplicationDelegate
+    {
+        protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+    }
+}
---
+++ MauiPopupsApp/ViewModels/MainPageViewModel.cs
@@ -0,0 +1,19 @@
+﻿using CommunityToolkit.Mvvm.Input;
+using MauiPopupsApp.Views;
+
+namespace MauiPopupsApp.ViewModel
+{
+    public partial class MainPageViewModel
+    {
+        public MainPageViewModel()
+        {
+        }
+
+        [RelayCommand]
+        public async Task DisplayMopup()
+        {
+            var fileData = await FilePicker.PickAsync();
+        }
+
+    }
+}
